49dba2e56ad12510df0eb9830cf22833db7d6eb8
Roberto Macedo <robertomacedo3007@gmail.com>
2023-09-12 22:06:08 -0300

    New Commit

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,27 @@
+﻿using Fiap.Web.Donation1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace Fiap.Web.Donation1.Controllers
+{
+    public class ContatoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Gravar(ContatoModel contatoModel)
+        {
+            return View("Sucesso");
+        }
+
+        [HttpGet]
+        public IActionResult Help()
+        {
+            return View();
+        }
+    }
+}
